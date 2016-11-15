219edf234091d7eba0a49ca48c9a00f8810987a8
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-11-15 17:41:47 +0100

    knx

---
+++ 07 - KNX/KNX/KNX/KNXCommunicator.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace KNXDemo
+    {
+    public class KNXCommunicator
+        {
+        public event EventHandler<KNXMessageEventArgs> KNXMessage;
+        //private TcpClient client;
+        private string _ipadres;
+        private int _port;
+        private UIElement _uieParent;   //nodig om terug te keren naar de ui thread
+        private StreamWriter writer;
+        private NetworkStream output;
+        private StreamReader reader;
+        private Boolean _gesloten = true;
+
+        // ' thread prevents client from blocking data transfer
+        private Thread readThread;
+
+        public KNXCommunicator(string IPAdres, int port, UIElement uieParent)
+            {
+            _ipadres = IPAdres;
+            _port = port;
+            _uieParent = uieParent;
+            InitClient();
+            }
+
+        private void InitClient()
+            {
+            readThread = new Thread(RunClient);
+            readThread.Start();
+            }
+        public void ZendMSG(string msg)
+            {
+            //eventueel gesloten door een aanpassing in de knx omgeving
+            if (IsGesloten) InitClient();
+
+            writer.WriteLine(msg);
+            writer.Flush();
+            }
+
+        public  bool IsGesloten { get { return _gesloten; } }
+
+
+        protected virtual void OnKNXMessage(string msg)
+            {
+            if (KNXMessage != null)
+                {
+                KNXMessage(this, new KNXMessageEventArgs(msg));
+                }
+            }
+
+        public delegate void OnKNXMessageDelegate(string msg);
+        public void RunClient()
+            {
+            TcpClient client = new TcpClient(); ;
+            // ' instantiate TcpClient for sending data to server
+            try
+                {
+                //' Step 1: create TcpClient and connect to server
+                client.Connect(_ipadres, _port);
+
+                //' Step 2: get NetworkStream associated with TcpClient
+                output = client.GetStream();
+
+                //' create objects for writing and reading across stream
+                writer = new StreamWriter(output);
+                reader = new StreamReader(output);
+
+                string message = "";
+                try
+                    {
+                    //' loop until server signals termination
+                    while (!message.Trim().ToLower().Equals("server>>>terminate"))
+                        //' read message from server
+                        try
+                            {
+                            message = reader.ReadLine();
+                            Console.WriteLine(message);
+                            OnKNXMessageDelegate del = new OnKNXMessageDelegate(OnKNXMessage);
+                            //OnKNXMessage(message);
+                            _uieParent.Dispatcher.BeginInvoke(del, new object[] { message });
+                            }
+                        catch (Exception ex)
+                            {
+                            }
+                    }
+
+       //' handle exception if error in reading server data
+                catch (IOException inputOutputException)
+                    {
+                    _gesloten = true;
+                    MessageBox.Show("Client application closing: " + inputOutputException.Message);     //reconnect triggeren
+                    }
+
+      // ' Step 4: close connection
+                finally
+                    {
+                    _gesloten = true;
+                    Console.WriteLine("closing connection");
+                    writer.Close();
+                    reader.Close();
+                    output.Close();
+                    client.Close();
+                    }
+                }
+
+    // ' handle exception if error in establishing connection
+            catch (Exception inputOutputException)
+                {
+                _gesloten = true;
+                MessageBox.Show("Client application closing");
+                }
+            }
+        }
+    public class KNXMessageEventArgs : EventArgs
+        {
+        private string _msg;
+        public KNXMessageEventArgs(string msg) { _msg = msg; }
+        public String Message { get { return _msg; } }
+        }
+    }
---
+++ 07 - KNX/KNX/KNX/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using KNXDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KNX
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KNXCommunicator _communicator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _communicator = new KNXCommunicator("172.23.49.0", 1289, lstOntvangen);
+            _communicator.RunClient();
+            _communicator.KNXMessage += _communicator_KNXMessage;
+        }
+
+        private void _communicator_KNXMessage(object sender, KNXMessageEventArgs e)
+        {
+            lstOntvangen.Items.Add(e.Message);
+        }
+    }
+}
