cb2da19e92c6d54d8c7a8e3e6cdda780909b7e0c
Hein Pauwelyn <hein.pauwelyn-vandewielle@student.howest.be>
2016-11-15 17:25:32 +0100

    lua update

--- 07 - KNX/tcpserver.lua
+++ 07 - KNX/tcpserver.lua
@@ -17,30 +17,48 @@ if not ready then
       sock:send('HELLO TO YOU! \r\n')
     end
     
-    if data == 'stijn' then
-      sock:send('muhahaha stijn! \r\n')
+    
+     -- hbt  send write 1 to group
+    if data == 'ON' then
+      sock:send('write on group\n')
+    	grp.write('4/0/0', true)
     end
     
-    -- groepsfunctie aanroepen  
-    if(string.match(data, "alex")) then
+    if data == 'OFF' then
+      grp.write('4/0/0', false)
+      sock:send('write on group\n')
+    end
+    
+    alert(data)
+    if string.match(data, "alex") then
       alert('alex matched')
       kxnAlex(sock,data)
+    else
+      knxHein(sock,data)
+      alert('alex not matched')
     end
     
-    knxsander(sock,data)
-    knxhenk(sock,data)
-    groep1(sock,data)
-    knxmilan(sock,data)
-    knxjordy(sock, data)
-    knxSanderVerkaemer(sock,data)
-    knxthomas(sock,data)
-    knxhein(sock,data)
-    
-    sock:send('This is no SPAM!/r/n   -- Anonymous/r/n')
-    
-    -- einde groepsfunctie
-    
   end
+  
+  function knxAlex(sock,data)
+    if string.match(data, "set") then
+      alert('set data')
+    else
+      alert('do not set data')
+    end
+  end
+  
+  function knxHein(sock,data)
+    -- send reply
+    if data == 'blij' then
+      sock:send('framblij /r/n')
+    end
+    
+    if data == 'boos' then
+      sock:send('framboos /r/n')
+    end
+  end
+  
  -- connection handler
   function connhandler(sock)
     -- enable keep-alive to check for disconnect events
@@ -77,129 +95,15 @@ if not ready then
       datahandler(sock, data)
     end
   end
-  
-   
+ 
   -- bind to port 12345
-  tcpserver = socket.bind('*', 12345)
+  tcpserver = socket.bind('*', 123459)
  
   -- error while binding, try again later
   if not tcpserver then
     os.sleep(5)
     error('[server] error: cannot bind')
   end
-  
-  -- start groepfuncties
-  function knxhein(sock,data)
-    -- send reply
-    if data == 'BLIJ' then
-      sock:send('framblij /r/n')
-    end
-    
-    if data == 'BOOS' then
-      sock:send('framboos /r/n')
-    end
-    
-    sock:send(data + ' /r/n')
-  end
- 
-  
-  
-  function knxhenk(sock,data)
-    -- send reply
-    if data == 'HENK' then
-      sock:send('HELLO TO YOU HENK! \r\n')
-    end
-    
-       -- hbt  send write 1 to group
-    if data == 'ON' then
-      sock:send('write on group\n')
-    	grp.write('4/0/0', true)
-    end
-    
-    if data == 'OFF' then
-      grp.write('4/0/0', false)
-      sock:send('write on group\n')
-    end
-      
-      if data == 'temperatuur' then
-        sock:send('read on group\n')
-        grp.read('1/0/0')
-      end
-  end
-  
-    -- start groepfuncties
-  function knxsander(sock,data)
-    -- send reply
-    if data == 'SANDER' then
-      sock:send('HELLO TO YOU stijn VERVAEKE! \r\n')
-    end
-    
-       -- hbt  send write 1 to group
-    if data == 'ON' then
-      sock:send('write on group\n')
-    	grp.write('4/0/0', true)
-    end
-    
-    if data == 'OFF' then
-      grp.write('4/0/0', false)
-      sock:send('write on group\n')
-    end
-      
-      if data == 'temperatuur' then
-        sock:send('read on group\n')
-        grp.read('1/0/0')
-      end
-  end
-  
-  function knxmilan(sock,data)
-    -- send reply
-    if data == 'MILAN' then
-      sock:send('WELKOM MILAN \r \n')
-    end
-  end
-  
-  function knxjordy(sock,data)
-    -- send reply
-    if data == 'JORDY' then
-      sock:send('WELKOM JORDY \r \n')
-    end
-  end
-  
-      -- start mijn functie
-  function knxthomas(sock, data)
-      -- send reply
-      if data == 'thomas' then
-      sock:send('Hallo Thomas, welkom op de knx. /r/n')
-      end
-  end
-  
-    function groep1(sock,data)
-      
-    end
-  
-  function knxAlex(sock,data)
-      
-  end
-  
-  function knxSanderVerkaemer(sock,data)
-    -- send reply
-    if data == 'VERKAEMER' then
-      sock:send('HELLO stijn! \r\n')
-    end
-    
-       -- hbt  send write 1 to group
-    if data == '15-ON' then
-      sock:send('write on group\n')
-    	grp.write('4/0/0', true)
-    end
-    
-    if data == '15-OFF' then
-      grp.write('4/0/0', false)
-      sock:send('write on group\n')
-    end
-      
-  end
-    -- einde groepsfuncties
 
   -- set server connection handler
   copas.addserver(tcpserver, connhandler)
